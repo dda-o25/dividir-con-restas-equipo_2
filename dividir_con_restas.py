5f285354aa3ba7a68d50ee4a3b5f4a237496d669
Adlr1307 <andres.jaime@iteso.mx>
2025-10-07 16:32:47 -0600

    Primera versión

--- dividir_con_restas.py
+++ dividir_con_restas.py
@@ -1,15 +1,28 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Andrés Enrique Jaime de la Rosa 763799  
+
+Emiliano Mayorga Álvarez expediente 764147  
+
+Julieta Núñez Pacheco 764729  
+
+Sol Valeria González Castro 764533  
+
+El propósito de este código es calcular el cociente y residuo de una divison por medio de sumas y restas
+07/10/2025
 """
 
 # Declaraciones
-CONSTANTE = valor
+cociente = 0
+residuo = 0
 
 # Entradas
-entrada = input()
+dividendo = float(input("Introduzca el dividendo: "))
+divisor = float(input("Introduzca el divisor: "))
 
 # Proceso
-
-
+while dividendo>=divisor:
+    dividendo = dividendo-divisor
+    residuo = dividendo
+    cociente = cociente +1
 # Salidas
-print(salida)
+print(f"El cociente es {cociente}\nEl residuo es {residuo}")
